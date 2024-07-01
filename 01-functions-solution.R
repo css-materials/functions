793485c6253a2093539c4920d9b42c80d6a1ac2c
Sabrina Nardin <nardin@uchicago.com>
2024-06-30 23:19:18 -0500

    improved soln

--- 01-functions-solution.R
+++ 01-functions-solution.R
@@ -1,37 +1,77 @@
-# practice working with functions - solutions
+# PRACTICE WRITING FUNCTIONS -- SOLUTIONS
+
+# Summary
+# QUESTIONS 1 AND 2: practice functions' logic and syntax
+# QUESTION 3: practice writing anonymous functions
+# QUESTION 4: practice conditional returns
+# QUESTION 5: practices loops, conditionals, and stop() statements in a function
 
 
-## Question 1. Perform the sum (number 1 + number 2) of the numbers 10 and 20, and divide it by 2 
-## Write the code without a function
-num1 <- 10
-num2 <- 20
-(num1 + num2) / 2
+# QUESTION 1. Sum the numbers 10 and 20, and divide the total by 2 
+# Write the code without a function
+# Re-write it with a function
 
+# Without, option 1
+result <- (10 + 20) / 2
 
-## Question 2. Write the same code in a function and call it with a few examples
-add_two <- function(num1, num2){
-  return((num1 + num2)/2)
+# Without, option 2
+number1 <- 10
+number2 <- 20
+(number1 + number2) / 2
+
+# with function
+add_two <- function(number1, number2) {
+  return((number1 + number2)/2)
 }
-# Call the function
 add_two(10, 20)
 add_two(5, 7)
 
-## Same code without the return statement 
-add_two <- function(num1, num2){
-  (num1 + num2)/2
+
+# QUESTION 2. Write code to convert 25 degree Celsius to Fahrenheit 
+# using this formula: fahrenheit = (celsius * 9/5) + 32
+# Write the code without a function
+# Re-write it with a function
+
+# Without, option 1
+fahr <- (25 * 9/5) + 32
+
+# Without, option 2
+celsius <- 25
+fahr <- (celsius * 9/5) + 32
+
+# With a function:
+celsius_to_fahr <- function(celsius) {
+  return((celsius * 9/5) + 32)
 }
-# Call the function
-add_two(10, 20)
-add_two(5, 7)
+celsius_to_fahr(25)
 
 
-# Question 3. Write a fizz buzz function that takes a single number as input
-# hint: you need to use modular %% division and if, else if, else statements
 
-# If the number is divisible by three, it returns "fizz"
-# If it’s divisible by five it returns "buzz"
-# If it’s divisible by three and five, it returns "fizzbuzz"
-# Otherwise, it returns the number
+# QUESTION 3. Take the function from question 2 (Celsius to Fahrenheit).
+# First, rewrite it as anonymous function using the general anonymous function syntax
+# Then, rewrite it as anonymous function with the purrr syntax with map_dbl() 
+
+# anonymous function syntax
+(function(x) x * 9/5 + 32) (25)
+
+# using using `purrr` syntax
+library(tidyverse)
+map_dbl(25, ~.x * 9/5 + 32)
+
+# if you pass a vector as input instead than a scalar...
+# this does not work
+(function(x) x * 9/5 + 32) (25, 20, 15)
+# this works
+sapply(c(25, 20, 15), function(x) x * 9/5 + 32)
+
+
+
+# QUESTION 4. Write a function that takes a single number as input, and:
+# if the number is divisible by 3 and 5, the function returns "fizzbuzz"
+# if the number is divisible by 3, it returns "fizz"
+# if the number is divisible by 5, it returns "buzz"
+# otherwise, the function returns the number
+# Hint: use modular %% division and if, else if, else statements
 
 fizzbuzz <- function(x){
   if(x %% 3 == 0 & x %% 5 == 0){
@@ -47,3 +87,63 @@ fizzbuzz <- function(x){
 
 fizzbuzz(3)
 fizzbuzz(28)
+
+
+# QUESTION 5. Write a function that calculates the mean of a numeric vector.
+# Do not use any R built-in functions, e.g., do not use length(), sum(), etc.
+# Then add a check to evaluate if the input vector is numeric
+# If not, use stop() to stop the code and write a message
+
+# Solution without built-in functions
+mean_vect <- function(vector) {
+  
+  # initialize variables
+  sum_elements <- 0
+  count_elements <- 0
+  
+  # calculate sum of elements in vector
+  for (v in vector) {
+    sum_elements <- sum_elements + v
+  }
+   
+  # calculate length of vector 
+  for (v in vector) {
+    count_elements <- count_elements + 1 
+  }
+  
+  # calculate the mean
+  mean_value <- sum_elements / count_elements
+  return(mean_value)
+}
+
+# call the function
+a <- c(2,4,6)
+mean_vect(a)
+
+
+# Add stop() and rewrite previous function more succinctly
+# and loop over vector indices vs directly on its elements 
+
+mean_vect <- function(vector) {
+  
+  # check if vector is numeric and stop()
+  if (!is.numeric(vector)) {
+    stop("Input vector must be numeric!")
+  }
+  
+  sum_elements <- 0
+  count_elements <- 0
+  
+  for (i in seq_along(vector)) {
+    sum_elements <- sum_elements + vector[i]
+    count_elements <- count_elements + 1
+  }
+  return(sum_elements / count_elements)
+}
+
+# call the function
+a <- c(2, 4, 6)
+mean_vect(a)
+
+b <- c(2, 4, "6")
+mean_vect(b)
