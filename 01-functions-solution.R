d7184ac97a5a02f17e6a294c18f3bb6dbf629edf
Sabrina Nardin <nardin@uchicago.com>
2024-07-01 09:21:48 -0500

    improved and expanded exercises

--- 01-functions-solution.R
+++ 01-functions-solution.R
@@ -5,6 +5,9 @@
 # QUESTION 3: practice writing anonymous functions
 # QUESTION 4: practice conditional returns
 # QUESTION 5: practices loops, conditionals, and stop() statements in a function
+# QUESTION 6: practice writing documentation
+# QUESTION 7: practice using {{}}
+
 
 
 # QUESTION 1. Sum the numbers 10 and 20, and divide the total by 2 
@@ -25,6 +28,7 @@ add_two <- function(number1, number2) {
 }
 add_two(10, 20)
 add_two(5, 7)
+
 
 
 # QUESTION 2. Write code to convert 25 degree Celsius to Fahrenheit 
@@ -89,6 +93,7 @@ fizzbuzz(3)
 fizzbuzz(28)
 
 
+
 # QUESTION 5. Write a function that calculates the mean of a numeric vector.
 # Do not use any R built-in functions, e.g., do not use length(), sum(), etc.
 # Then add a check to evaluate if the input vector is numeric
@@ -147,3 +152,36 @@ mean_vect(a)
 
 b <- c(2, 4, "6")
 mean_vect(b)
+
+
+
+# QUESTION 6. Add the documentation the function you wrote in question 4 or in 5.
+
+
+
+# QUESTION 7. Given this plot, rewrite it as a function.
+# Your function should take three arguments like that (df, var, binwidth = NULL)
+
+# import library and data
+library(tidyverse)
+data(diamonds)
+
+# code to re-write 
+diamonds %>% 
+  ggplot(aes(x = carat)) + 
+           geom_histogram(binwidth = 0.1)
+
+# solution
+hist <- function(df, var, binwidth = NULL) {
+  df %>% 
+  ggplot(aes(x = {{ var }})) + 
+  geom_histogram(binwidth = binwidth)
+}
+
+# call the function (both codes work)
+hist(diamonds, carat, 0.1)
+diamonds %>% hist(carat, 0.1)
+
+# can also add labels
+diamonds %>% hist(carat, 0.1) +
+  labs(x = "Size (in carats)", y = "Number of diamonds")
--- 01-functions.R
+++ 01-functions.R
@@ -1,23 +1,72 @@
-# practice working with functions - solutions
+# PRACTICE WRITING FUNCTIONS -- SOLUTIONS
 
-
-## Question 1. Perform the sum (number 1 + number 2) of the numbers 10 and 20, and divide it by 2 
-## Write the code without a function
-
-
-
-
-## Question 2. Write the same code in a function and call it with a few examples
-
+# Summary
+# QUESTIONS 1 AND 2: practice functions' logic and syntax
+# QUESTION 3: practice writing anonymous functions
+# QUESTION 4: practice conditional returns
+# QUESTION 5: practices loops, conditionals, and stop() statements in a function
+# QUESTION 6: practice writing documentation
+# QUESTION 7: practice using {{}}
 
 
 
-## Question 3. Write a fizz buzz function that takes a single number as input
-# If the number is divisible by three, it returns "fizz"
-# If it’s divisible by five it returns "buzz"
-# If it’s divisible by three and five, it returns "fizzbuzz"
-# Otherwise, it returns the number
-# Hint: you need to use modular %% division and if, else if, else statements
+# QUESTION 1. Sum the numbers 10 and 20, and divide the total by 2 
+# Write the code without a function
+# Re-write it with a function
+
+
+
+
+# QUESTION 2. Write code to convert 25 degree Celsius to Fahrenheit 
+# using this formula: fahrenheit = (celsius * 9/5) + 32
+# Write the code without a function
+# Re-write it with a function
+
+
+
+
+# QUESTION 3. Take the function from question 2 (Celsius to Fahrenheit).
+# First, rewrite it as anonymous function using the general anonymous function syntax
+# Then, rewrite it as anonymous function with the purrr syntax with map_dbl() 
+
+
+
+
+# QUESTION 4. Write a function that takes a single number as input, and:
+# if the number is divisible by 3 and 5, the function returns "fizzbuzz"
+# if the number is divisible by 3, it returns "fizz"
+# if the number is divisible by 5, it returns "buzz"
+# otherwise, the function returns the number
+# Hint: use modular %% division and if, else if, else statements
+
+
+
+
+# QUESTION 5. Write a function that calculates the mean of a numeric vector.
+# Do not use any R built-in functions, e.g., do not use length(), sum(), etc.
+# Then add a check to evaluate if the input vector is numeric
+# If not, use stop() to stop the code and write a message
+
+
+
+
+# QUESTION 6. Add the documentation the function you wrote in question 4 or in 5.
+
+
+
+
+# QUESTION 7. Given this plot, rewrite it as a function.
+# Your function should take three arguments like that (df, var, binwidth = NULL)
+
+# import library and data
+library(tidyverse)
+data(diamonds)
+
+# code to re-write 
+diamonds %>% 
+  ggplot(aes(x = carat)) + 
+  geom_histogram(binwidth = 0.1)
+
 
 
 
